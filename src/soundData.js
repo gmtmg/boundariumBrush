a7c85f10abd3296c1cd1cd07d86d74cd0b3520ef
i <ajimi.ruru@gmail.com>
2025-05-18 22:15:05 +0900

    初回

---
+++ src/soundData.js
@@ -0,0 +1,79 @@
+//
+////const scaleColors = ["#FF0000", "#FFFF00", "#00FF00", "#FFA500", "#0000FF", "#800080", "#FFC0CB","#FF0000"];
+//
+//const freq = {
+//  beep : {
+//    do1: 261.63, re1: 293.66, mi1: 329.63, fa1: 349.23, so1: 392.50, so_ra_1: 417, ra1: 440.00, shi1: 493.88, do2: 523.25
+//  },
+//  piano : {
+//    do1  :"soundSource/do1_piano.mp3",
+//    re1  :"soundSource/re1_piano.mp3",
+//    mi1  :"soundSource/mi1_piano.mp3",
+//    fa1  :"soundSource/fa1_piano.mp3",
+//    so1  :"soundSource/so1_piano.mp3",
+//    ra1  :"soundSource/ra1_piano.mp3",
+//    shi1 :"soundSource/shi1_piano.mp3",
+//    do2  :"soundSource/do2_piano.mp3",
+//  },
+//  darkpiano : {
+//    do1  :"soundSource/do1_darkpiano.mp3",
+//    re1  :"soundSource/re1_darkpiano.mp3",
+//    mi1  :"soundSource/mi1_darkpiano.mp3",
+//    fa1  :"soundSource/fa1_darkpiano.mp3",
+//    so1  :"soundSource/so1_darkpiano.mp3",
+//    ra1  :"soundSource/ra1_darkpiano.mp3",
+//    shi1 :"soundSource/shi1_darkpiano.mp3",
+//    do2  :"soundSource/do2_darkpiano.mp3",
+//  },
+//  novelClav : {
+//    do1  :"soundSource/do1_novelClav.mp3",
+//    re1  :"soundSource/re1_novelClav.mp3",
+//    mi1  :"soundSource/mi1_novelClav.mp3",
+//    fa1  :"soundSource/fa1_novelClav.mp3",
+//    so1  :"soundSource/so1_novelClav.mp3",
+//    ra1  :"soundSource/ra1_novelClav.mp3",
+//    shi1 :"soundSource/shi1_novelClav.mp3",
+//    do2  :"soundSource/do2_novelClav.mp3",
+//  },
+//  vibp : {
+//    do1  :"soundSource/do1_vibp.mp3",
+//    re1  :"soundSource/re1_vibp.mp3",
+//    mi1  :"soundSource/mi1_vibp.mp3",
+//    fa1  :"soundSource/fa1_vibp.mp3",
+//    so1  :"soundSource/so1_vibp.mp3",
+//    ra1  :"soundSource/ra1_vibp.mp3",
+//    shi1 :"soundSource/shi1_vibp.mp3",
+//    do2  :"soundSource/do2_vibp.mp3",
+//  },
+//  drum : {
+//    do1  :"soundSource/drum.mp3",
+//    re1  :"soundSource/drum.mp3",
+//    mi1  :"soundSource/drum.mp3",
+//    fa1  :"soundSource/drum2.mp3",
+//    so1  :"soundSource/drum2.mp3",
+//    ra1  :"soundSource/drum2.mp3",
+//    shi1 :"soundSource/drum3.mp3",
+//    do2  :"soundSource/drum3.mp3",
+//  }
+//};
+
+const freq = {
+  vibp: {
+    c: "soundSource/do1_vibp.mp3",
+    d: "soundSource/re1_vibp.mp3",
+    e: "soundSource/mi1_vibp.mp3",
+    f: "soundSource/fa1_vibp.mp3",
+    g: "soundSource/so1_vibp.mp3",
+    a: "soundSource/ra1_vibp.mp3",
+    b: "soundSource/shi1_vibp.mp3",
+  },
+  piano: {
+    c: "soundSource/do1_piano.mp3",
+    d: "soundSource/re1_piano.mp3",
+    e: "soundSource/mi1_piano.mp3",
+    f: "soundSource/fa1_piano.mp3",
+    g: "soundSource/so1_piano.mp3",
+    a: "soundSource/ra1_piano.mp3",
+    b: "soundSource/shi1_piano.mp3",
+  }
+};
